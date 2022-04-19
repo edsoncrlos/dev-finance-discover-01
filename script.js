e78d9ef842dcf08408293c108ebdec58e1828cb3
Edson Carlos <edsoncarlos.dev@gmail.com>
2022-04-19 15:30:25 -0300

    adding the cards via javascript

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// TO-DO toogle
+
 const Modal = {
     open() {
         document.
@@ -8,3 +10,64 @@ const Modal = {
         querySelector('.modal-overlay').classList.remove('active')
     }
 }
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021',
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021',
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021',
+    },
+]
+
+const Trasaction = {
+    incomes() {
+        // Somar as entradas
+    },
+    expenses() {
+        // Somar as saídas
+    },
+    total() {
+        // entradas - saídas
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount >= 0 ? "income" : "expense"
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover transação">
+            </td>
+        `
+
+        return html
+    }
+}
+
+transactions.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+}) 
