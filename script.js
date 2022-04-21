c2d60e4a94d5aead7ca41785eff7bcab29da4302
Edson Carlos <edsoncarlos.dev@gmail.com>
2022-04-21 09:11:29 -0300

    adding Form Object and function validateFields

--- script.js
+++ script.js
@@ -136,6 +136,41 @@ const Utils = {
     }
 }
 
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+
+    validateFields() {
+        const {description, amout, date} = Form.getValues()
+
+        if(description.trim() === "" || amount.trim() === "" 
+        || date.trim() === "") {
+            throw new Error("Por favor, preencha todos os campos")
+        }
+    },
+
+    submit(event) {
+        event.preventDefault()
+
+        try {
+            Form.validateFields()
+        } catch (error) {
+            alert(error.message)
+        }
+
+        // Form.formatData()
+    }
+}
+
 const App = {
     init() {
         Transaction.all.forEach(transaction => {
