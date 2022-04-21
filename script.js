25e02abce54db233d45a8b18b88fcf8f34c16a1c
Edson Carlos <edsoncarlos.dev@gmail.com>
2022-04-21 11:19:54 -0300

    :bug: adding the index in function

--- script.js
+++ script.js
@@ -209,8 +209,8 @@ const Form = {
 
 const App = {
     init() {
-        Transaction.all.forEach(transaction => {
-            DOM.addTransaction(transaction)
+        Transaction.all.forEach((transaction, index) => {
+            DOM.addTransaction(transaction, index)
         })
 
         DOM.updateBalance()
